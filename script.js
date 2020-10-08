db70ef543f98cbea47b3d0ba3a8a0ef6d3a6051d
leilafreitas <leilafreitas159@gmail.com>
2020-10-08 09:19:02 -0300

    urna eletronica javascript

---
+++ script.js
@@ -0,0 +1,107 @@
+let seu_voto_para=document.querySelector('.d-1-1 span');
+let cargo=document.querySelector('.d-1-2 span');
+let num=document.querySelector('.d-1-3');
+let descricao=document.querySelector('.d-1-4');
+let aviso = document.querySelector('.d-2');
+let fotos= document.querySelector('.d-1-right');
+//VARIÁVEIS DE CONTROLE DE AMBIENTE
+let etapaAtual=0;
+let numero='';
+let vbranco=false
+function iniciarEtapa(){
+    let etapa =etapas[etapaAtual];
+    numero='';
+    vbranco=false;
+    let numeroHTML='';
+
+    for(let i=0;i<etapa.numeros;i++){
+        if(i === 0){
+            numeroHTML += '<div class="numero pisca"></div>';
+        }else{
+            numeroHTML += '<div class="numero"></div>';
+        }
+        
+    }
+    cargo.innerHTML=etapa.titulo;
+    descricao.innerHTML='';
+    seu_voto_para.style.display='none';
+    fotos.innerHTML='';
+    num.innerHTML=numeroHTML;
+    aviso.style.display='none';
+}
+function atualizaInterface(){
+    console.log('ENTROU');
+    let etapa=etapas[etapaAtual];
+    let candidato=etapa.candidatos.filter((item)=>{
+        if(item.numero===numero){
+            return true;
+        }else{
+            return false;
+        }
+    });
+    //filter retorna um array, por isso colocamos que candidato=candidato[0]
+    if(candidato.length >0){
+        candidato=candidato[0];
+        descricao.innerHTML=`Nome:${candidato.nome}<br/>
+        Partido: ${candidato.partido}<br/>`;
+        seu_voto_para.style.display='block';
+        let fotosHTML=''
+        for(let i of candidato.fotos){
+            if(i.small){
+                fotosHTML +=`<div class="d-1-image small"><img src="images/${i.url}" alt="">${i.legenda}</div>`
+
+            }else{
+                fotosHTML +=`<div class="d-1-image"><img src="images/${i.url}" alt="">${i.legenda}</div>`
+            }
+        }
+        fotos.innerHTML=fotosHTML;
+        aviso.style.display='block';
+    }else{
+        seu_voto_para.style.display='block';
+        aviso.style.display='block';
+        descricao.innerHTML=`<div class="aviso--grande pisca">VOTO NULO</div>`
+    }
+
+
+}
+function clicou(n){
+    let numer = document.querySelector('.numero.pisca');
+
+    if(numer != null){
+        numer.innerHTML = n;
+        numero=`${numero}${n}`;
+    }
+    numer.classList.remove('pisca');
+    //ACHA O ELEMENTO DO LADO
+    if(numer.nextElementSibling !== null){
+        numer.nextElementSibling.classList.add('pisca');
+    }else{
+        atualizaInterface();
+    }
+}
+function branco(){
+        numero='';
+        vbranco=true;
+        seu_voto_para.style.display='block';
+        aviso.style.display='block';
+        num.innerHTML='';
+        fotos.innerHTML='';
+        descricao.innerHTML=`<div class="aviso--grande pisca">VOTO EM BRANCO</div>`
+}
+function corrige(){
+    iniciarEtapa();
+    
+}
+function confirma(){
+    let etapa=etapas[etapaAtual];
+    if(vbranco===true || numero.length===etapa.numeros){
+        etapaAtual++;   
+    }
+    if(etapas[etapaAtual] !== undefined){
+        iniciarEtapa();
+    }else{
+        alert('VOTOS REGISTRADOS');
+    }
+    
+}
+iniciarEtapa();
